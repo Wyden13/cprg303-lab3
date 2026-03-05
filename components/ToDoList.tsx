fb26f6409100add7c197863743933feab4b023a9
wendy nguyen <uyennhp134@gmail.com>
2026-03-05 09:09:47 -0700

    remove: a file

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScrollView, Pressable, View, Text } from "react-native";
 import { StyleSheet } from "react-native";
+
 export default function ToDoList({ tasks }: { tasks: string[] }) {
   return (
     <ScrollView>
